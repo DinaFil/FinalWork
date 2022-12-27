ce09d8edc055e7240be368ed74b0ab547e2f7743
Dina Filippova <dinafilippova@gmail.com>
2022-12-27 17:30:30 +0700

    Method LessThenThreeArray created

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿Console.WriteLine("Введите массив строк через пробел:");
 string text = (Console.ReadLine());
 string[] collection = text.Split(" ");
+
+
+void LessThenThreeArray(string[] phrase)
+{
+string[] newphrase = new string[phrase.Length];
+for (int i = 0; i < phrase.Length; i++)
+{
+if (phrase[i].Length < 4) 
+{
+newphrase[i] = phrase[i];
+Console.Write(phrase[i] + " ");
+}
+}
+}
